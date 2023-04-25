c91964e71e78ff6ed6382d1b1d4d5eea2bf1cda8
SHIVENDRA8004 <jat13632@gmail.com>
2023-04-25 23:48:11 +0530

    Created a constants file to store all contants like posts

---
+++ src/components/constants/constants.js
@@ -0,0 +1,13 @@
+import image1 from "../../assets/post/post1_img.png";
+import image2 from "../../assets/post/post2_img.png";
+import image3 from "../../assets/post/post3_img.png";
+const posts = [
+  {
+    postImage: image1,
+    postType: "✍Article",
+    postTitle: "What if famous brands had regular fonts? Meet RegulaBrands!",
+    postBody: "I've worked in UX for the better part of a decade. From now on, I plan to rei…",
+    postUser: "Sarthak Kamra",
+    postViews: "1.4k",
+  },
+];
