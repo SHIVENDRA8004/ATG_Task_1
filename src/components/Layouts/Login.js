a856a9bc69bbce974546750e567724d0250439cf
SHIVENDRA8004 <jat13632@gmail.com>
2023-04-25 17:37:41 +0530

    Login page done

---
+++ src/components/Layouts/Login.js
@@ -0,0 +1,103 @@
+import fbLogo from "../../assets/fbLogo.svg";
+import googleLogo from "../../assets/googleLogo.svg";
+import illustration from "../../assets/atg_illustration.svg";
+import { useState } from "react";
+const Login = (props) => {
+  const showModal = props.showModal;
+  const [showPassword, setShowPassword] = useState("password");
+  return (
+    <div>
+      <div className="d-flex justify-content-end mb-2">
+        <i
+          class="bi bi-x-circle-fill cursor-pointer"
+          onClick={() => {
+            props.HandleRegisterClick(showModal);
+          }}
+        ></i>
+      </div>
+      <div>
+        <div
+          className="text-center "
+          style={{ backgroundColor: "#EFFFF4", borderRadius: "8px 8px 0 0", padding: "25px 0" }}
+        >
+          <p className=" m-0 " style={{ color: "#008A45", fontWeight: "500" }}>
+            Let's learn, share & inspire each other with our passion for computer engineering. Sign up now 🤘🏼
+          </p>
+        </div>
+        <div className="container-fluid">
+          <div className="row">
+            <div className="col-6">
+              <p className="fs-3 fw-bold text-black mt-3 mb-3">Sign In</p>
+              <div className="container-fluid">
+                <div className="row">
+                  <input
+                    className="col-12 ps-3 border border-bottom-0 border-secondary"
+                    type="text"
+                    placeholder="Email"
+                  />
+                </div>
+                <div className="row">
+                  <input
+                    className="col-12 ps-3 border border-secondary"
+                    type={`${showPassword}`}
+                    placeholder="Password"
+                  />
+                  {showPassword === "password" ? (
+                    <i
+                      class="bi bi-eye position-absolute cursor-pointer"
+                      style={{ top: "225px", left: "300px" }}
+                      onClick={() => setShowPassword("text")}
+                    ></i>
+                  ) : (
+                    <i
+                      class="bi bi-eye-slash position-absolute cursor-pointer"
+                      style={{ top: "265px", left: "330px" }}
+                      onClick={() => setShowPassword("password")}
+                    ></i>
+                  )}
+                </div>
+                <div className="row">
+                  <button className="col-12 bg-primary rounded-pill border-0 text-light mt-3 mb-3 p-2">Sign In</button>
+                </div>
+                <div className="row">
+                  <button className="col-12 border border-bottom-0 border-secondary p-2">
+                    <img className="pe-2 pb-1" src={fbLogo} alt="Logo" /> Sign In with Facebook
+                  </button>
+                </div>
+                <div className="row">
+                  <button className="col-12 border border-secondary p-2 ">
+                    <img className="pe-2 pb-1" src={googleLogo} alt="Logo" />
+                    Sign In with Google
+                  </button>
+                </div>
+                <div className="row">
+                  <p className="text-center mt-3">Forgot Password?</p>
+                </div>
+              </div>
+            </div>
+            <div className=" container-fluid col-6">
+              <div className="row text-end">
+                <p className="col-12 mt-4" style={{ fontSize: "14px" }}>
+                  Don't have an account yet?
+                  <span className="text-primary fw-bold cursor-pointer" onClick={props.HandleSignupClick}>
+                    Create new for free!
+                  </span>
+                </p>
+              </div>
+              <div className="row ">
+                <img
+                  className="ms-auto me-auto"
+                  style={{ height: "320px", width: "320px" }}
+                  src={illustration}
+                  alt="Illustration"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
--- src/components/Layouts/Register.js
+++ src/components/Layouts/Register.js
@@ -2,122 +2,145 @@ import fbLogo from "../../assets/fbLogo.svg";
 import googleLogo from "../../assets/googleLogo.svg";
 import illustration from "../../assets/atg_illustration.svg";
 import { useState } from "react";
+import Login from "./Login";
 const Register = (props) => {
   const showModal = props.showModal;
   const [showPassword, setShowPassword] = useState("password");
+  const [registerClass, setRegisterClass] = useState("d-block");
+  const [loginClass, setLoginClass] = useState("d-none");
+  const HandleSignInClicK = () => {
+    setRegisterClass("d-none");
+    setLoginClass("d-flex");
+  };
+  const HandleSignupClick = () => {
+    setRegisterClass("d-block");
+    setLoginClass("d-none");
+  };
   return (
     <div
       className="bg-transparent position-absolute start-50 top-50 translate-middle w-50"
-      style={{ height: "513px", borderRadius: "8px" }}
+      style={{ borderRadius: "8px" }}
     >
-      <div className="d-flex justify-content-end">
-        <i
-          class="bi bi-x-circle-fill cursor-pointer"
-          onClick={() => {
-            props.HandleRegisterClick(showModal);
-          }}
-        ></i>
-      </div>
-      <div>
-        <div
-          className="text-center "
-          style={{ backgroundColor: "#EFFFF4", borderRadius: "8px 8px 0 0", padding: "25px 0" }}
-        >
-          <p className=" m-0 " style={{ color: "#008A45", fontWeight: "500" }}>
-            Let's learn, share & inspire each other with our passion for computer engineering. Sign up now 🤘🏼
-          </p>
+      <div className={`${registerClass}`}>
+        <div className="d-flex justify-content-end mb-2">
+          <i
+            class="bi bi-x-circle-fill cursor-pointer"
+            onClick={() => {
+              props.HandleRegisterClick(showModal);
+            }}
+          ></i>
         </div>
-        <div className="container-fluid">
-          <div className="row">
-            <div className="col-6">
-              <p className="fs-3 fw-bold text-black mt-3 mb-3">Create Account</p>
-              <div className="container-fluid">
-                <div className="row">
-                  <input
-                    className="ps-3 col-6 border border-end-0 border-bottom-0 border-secondary "
-                    type="text"
-                    placeholder="First Name"
-                  />
-                  <input
-                    className="col-6 ps-3 border border-bottom-0 border-secondary"
-                    type="text"
-                    placeholder="Last Name"
-                  />
-                </div>
-                <div className="row">
-                  <input
-                    className="col-12 ps-3 border border-bottom-0 border-secondary"
-                    type="text"
-                    placeholder="Email"
-                  />
-                </div>
-                <div className="row">
-                  <input
-                    className="col-12 ps-3 border border-bottom-0 border-secondary"
-                    type={`${showPassword}`}
-                    placeholder="Password"
-                  />
-                  {showPassword === "password" ? (
-                    <i
-                      class="bi bi-eye position-absolute cursor-pointer"
-                      style={{ top: "265px", left: "330px" }}
-                      onClick={() => setShowPassword("text")}
-                    ></i>
-                  ) : (
-                    <i
-                      class="bi bi-eye-slash position-absolute cursor-pointer"
-                      style={{ top: "265px", left: "330px" }}
-                      onClick={() => setShowPassword("password")}
-                    ></i>
-                  )}
-                </div>
-                <div className="row">
-                  <input
-                    className="col-12 ps-3 border border-secondary"
-                    type="password"
-                    placeholder="Confirm Password"
-                  />
-                </div>
-                <div className="row">
-                  <button className="col-12 bg-primary rounded-pill border-0 text-light mt-3 mb-3 p-2">
-                    Create Account
-                  </button>
-                </div>
-                <div className="row">
-                  <button className="col-12 border border-bottom-0 border-secondary p-2">
-                    <img className="pe-2 pb-1" src={fbLogo} alt="Logo" /> Sign up with Facebook
-                  </button>
-                </div>
-                <div className="row">
-                  <button className="col-12 border border-secondary p-2 ">
-                    <img className="pe-2 pb-1" src={googleLogo} alt="Logo" />
-                    Sign up with Google
-                  </button>
+        <div>
+          <div
+            className="text-center "
+            style={{ backgroundColor: "#EFFFF4", borderRadius: "8px 8px 0 0", padding: "25px 0" }}
+          >
+            <p className=" m-0 " style={{ color: "#008A45", fontWeight: "500" }}>
+              Let's learn, share & inspire each other with our passion for computer engineering. Sign up now 🤘🏼
+            </p>
+          </div>
+          <div className="container-fluid">
+            <div className="row">
+              <div className="col-6">
+                <p className="fs-3 fw-bold text-black mt-3 mb-3">Create Account</p>
+                <div className="container-fluid">
+                  <div className="row">
+                    <input
+                      className="ps-3 col-6 border border-end-0 border-bottom-0 border-secondary "
+                      type="text"
+                      placeholder="First Name"
+                    />
+                    <input
+                      className="col-6 ps-3 border border-bottom-0 border-secondary"
+                      type="text"
+                      placeholder="Last Name"
+                    />
+                  </div>
+                  <div className="row">
+                    <input
+                      className="col-12 ps-3 border border-bottom-0 border-secondary"
+                      type="text"
+                      placeholder="Email"
+                    />
+                  </div>
+                  <div className="row">
+                    <input
+                      className="col-12 ps-3 border border-bottom-0 border-secondary"
+                      type={`${showPassword}`}
+                      placeholder="Password"
+                    />
+                    {showPassword === "password" ? (
+                      <i
+                        class="bi bi-eye position-absolute cursor-pointer"
+                        style={{ top: "265px", left: "330px" }}
+                        onClick={() => setShowPassword("text")}
+                      ></i>
+                    ) : (
+                      <i
+                        class="bi bi-eye-slash position-absolute cursor-pointer"
+                        style={{ top: "265px", left: "330px" }}
+                        onClick={() => setShowPassword("password")}
+                      ></i>
+                    )}
+                  </div>
+                  <div className="row">
+                    <input
+                      className="col-12 ps-3 border border-secondary"
+                      type="password"
+                      placeholder="Confirm Password"
+                    />
+                  </div>
+                  <div className="row">
+                    <button className="col-12 bg-primary rounded-pill border-0 text-light mt-3 mb-3 p-2">
+                      Create Account
+                    </button>
+                  </div>
+                  <div className="row">
+                    <button className="col-12 border border-bottom-0 border-secondary p-2">
+                      <img className="pe-2 pb-1" src={fbLogo} alt="Logo" /> Sign up with Facebook
+                    </button>
+                  </div>
+                  <div className="row">
+                    <button className="col-12 border border-secondary p-2 ">
+                      <img className="pe-2 pb-1" src={googleLogo} alt="Logo" />
+                      Sign up with Google
+                    </button>
+                  </div>
                 </div>
               </div>
-            </div>
-            <div className=" container-fluid col-6">
-              <div className="row text-end">
-                <p className="col-12 mt-4" style={{ fontSize: "14px" }}>
-                  Already have an account?<span className="text-primary fw-bold cursor-pointer">Sign In</span>
-                </p>
-              </div>
-              <div className="row ">
-                <img
-                  className="ms-auto me-auto"
-                  style={{ height: "320px", width: "320px" }}
-                  src={illustration}
-                  alt="Illustration"
-                />
-              </div>
-              <div className="row ">
-                <p className="col-12 text-center" style={{ fontSize: "11px" }}>
-                  By signing up, you agree to our Terms & conditions, Privacy policy
-                </p>
+              <div className=" container-fluid col-6">
+                <div className="row text-end">
+                  <p className="col-12 mt-4" style={{ fontSize: "14px" }}>
+                    Already have an account?
+                    <span className="text-primary fw-bold cursor-pointer" onClick={HandleSignInClicK}>
+                      Sign In
+                    </span>
+                  </p>
+                </div>
+                <div className="row ">
+                  <img
+                    className="ms-auto me-auto"
+                    style={{ height: "320px", width: "320px" }}
+                    src={illustration}
+                    alt="Illustration"
+                  />
+                </div>
+                <div className="row ">
+                  <p className="col-12 text-center" style={{ fontSize: "11px" }}>
+                    By signing up, you agree to our Terms & conditions, Privacy policy
+                  </p>
+                </div>
               </div>
             </div>
           </div>
         </div>
+      </div>
+      <div className={`${loginClass}`}>
+        <Login
+          showModal={showModal}
+          HandleRegisterClick={props.HandleRegisterClick}
+          HandleSignupClick={HandleSignupClick}
+        />
       </div>
     </div>
   );
