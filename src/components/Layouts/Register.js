02aabbf9b29316db166b3da31044883f7f4af724
SHIVENDRA8004 <jat13632@gmail.com>
2023-04-25 10:42:55 +0530

    Created 1 section of register page

--- src/components/Layouts/Register.js
+++ src/components/Layouts/Register.js
@@ -1,9 +1,11 @@
+import fbLogo from "../../assets/fbLogo.svg";
+import googleLogo from "../../assets/googleLogo.svg";
 const Register = (props) => {
   const showModal = props.showModal;
   return (
     <div
-      className="bg-transparent position-absolute top-50 start-50 translate-middle"
-      style={{ height: "513px", width: "766px" }}
+      className="bg-transparent position-absolute start-50 top-50 translate-middle w-50"
+      style={{ height: "513px", borderRadius: "8px" }}
     >
       <div className="d-flex justify-content-end">
         <i
@@ -13,7 +15,7 @@ const Register = (props) => {
           }}
         ></i>
       </div>
-      <div className="mt-2" style={{ padding: "0 8px" }}>
+      <div>
         <div
           className="text-center "
           style={{ backgroundColor: "#EFFFF4", borderRadius: "8px 8px 0 0", padding: "25px 0" }}
@@ -28,17 +30,53 @@ const Register = (props) => {
               <p className="fs-3 fw-bold text-black mt-3 mb-3">Create Account</p>
               <div className="container-fluid">
                 <div className="row">
-                  <input className="ps-3 col-6 " type="text" placeholder="First Name" />
-                  <input className="col-6 ps-3" type="text" placeholder="Last Name" />
+                  <input
+                    className="ps-3 col-6 border border-end-0 border-bottom-0 border-secondary "
+                    type="text"
+                    placeholder="First Name"
+                  />
+                  <input
+                    className="col-6 ps-3 border border-bottom-0 border-secondary"
+                    type="text"
+                    placeholder="Last Name"
+                  />
                 </div>
                 <div className="row">
-                  <input className="col-12 ps-3" type="text" name="" id="" placeholder="Email" />
+                  <input
+                    className="col-12 ps-3 border border-bottom-0 border-secondary"
+                    type="text"
+                    placeholder="Email"
+                  />
                 </div>
                 <div className="row">
-                  <input className="col-12" type="password" name="" id="" placeholder="Password" />
+                  <input
+                    className="col-12 ps-3 border border-bottom-0 border-secondary"
+                    type="password"
+                    placeholder="Password"
+                  />
                 </div>
                 <div className="row">
-                  <input className="COL-12 ps-3" type="text" name="" id="" placeholder="Confirm Password" />
+                  <input
+                    className="col-12 ps-3 border border-secondary"
+                    type="password"
+                    placeholder="Confirm Password"
+                  />
+                </div>
+                <div className="row">
+                  <button className="col-12 bg-primary rounded-pill border-0 text-light mt-3 mb-3 p-2">
+                    Create Account
+                  </button>
+                </div>
+                <div className="row">
+                  <button className="col-12 border border-bottom-0 border-secondary p-2">
+                    <img className="pe-3 pb-1" src={fbLogo} alt="Logo" /> Sign up with Facebook
+                  </button>
+                </div>
+                <div className="row">
+                  <button className="col-12 border border-secondary p-2 ">
+                    <img className="pe-3 pb-1" src={googleLogo} alt="Logo" />
+                    Sign up with Google
+                  </button>
                 </div>
               </div>
             </div>
